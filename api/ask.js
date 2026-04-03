971217647519725661411b314919d3bd239e6f83
OnlineBunker <yash.dhankhar06@gmail.com>
2026-04-03 08:49:57 +0530

    idkanymore

--- api/ask.js
+++ api/ask.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
     });
 
     const result = await model.generateContent(
-      "Explain this in simple, normal English without headings or formatting:\n\n" +
+      "Explain this in simple, normal English without headings or formatting or give me the direct code :\n\n" +
         question,
     );
 
